233a8ee7aa6e781d79d69ebb8d1e3d172a47abff
jddelia <jddelia123@gmail.com>
2018-10-04 16:55:30 -0400

    Update functional_tests.py to include unittest module and user story.

--- functional_tests.py
+++ functional_tests.py
@@ -1,9 +1,44 @@
 from selenium import webdriver
+import unittest
 
-browser_firefox = webdriver.Firefox()
-browser_chrome = webdriver.Chrome()
+class NewVisitor(unittest.TestCase):
 
-browser_firefox.get("http://localhost:8000")
-browser_chrome.get("http://localhost:8000")
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        #self.browser = webdriver.Chrome()
 
-assert "Django" in browser_firefox
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_list_and_retrieve_laster(self):
+        # John heard about a new "to-do" list app,
+        # he goes to the website.
+        self.browser.get("http://localhost:8000")
+        #self.browser.get("http://localhost:8000")
+
+        # He notices that the word "To-Do" is in the title.
+        self.assertIn("To-Do", self.browser.title)
+        self.fail("Finish the test!")
+
+        # The app invites John to make an entry.
+
+        # He types "Study for exam".
+
+        # When hit hits enter, the page updates with the entry
+        # on the page as an item.
+
+        # The box for making an entry is still there, and John
+        # makes another entry: "Buy new shoes".
+
+        # He presses enter once more, and the page updates with
+        # that entry as an item in the list.
+
+        # John checks to see if the site will save his info, and
+        # notices that the site has generated a unique URL for him.
+
+        # He checks that URL, and it works.
+
+        # John is satisfied.
+
+if __name__ == '__main__':
+    unittest.main()
